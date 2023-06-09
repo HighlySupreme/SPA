88ebc60a70e0228bd0945026fb15363c3fcb329a
HighlySupreme <gal.1999k@gmail.com>
2023-06-09 11:50:36 +0200

    Added rest Created rest endpoints

--- src/services/rest.service.ts
+++ src/services/rest.service.ts
@@ -1,7 +1,15 @@
 import {JsonService} from "src/services/json.service";
 
 export const RestService = {
-    getUsers: () => JsonService.get('users'),
+    getUsers: () => JsonService.get(`users`),
 
+    getPosts: () => JsonService.get(`posts`),
 
+    getPost: (id: number) => JsonService.get(`posts/${id}`),
+
+    updatePost: (post: Post) => JsonService.put(`posts/${post.id}`, post),
+
+    deletePost: (id: number) => JsonService.delete(`posts/${id}`),
+
+    createPost: (post: Post) => JsonService.post(`posts`, post)
 };
