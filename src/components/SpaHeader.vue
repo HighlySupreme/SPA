3cecb5455ad580b16209065f69e73a38fe736cb6
HighlySupreme <gal.1999k@gmail.com>
2023-06-09 14:41:13 +0200

    SpaHeader Modifications to header

--- src/components/SpaHeader.vue
+++ src/components/SpaHeader.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="spa-header">
-        <div class="logo">
-            <q-icon name="mdi-account-cowboy-hat-outline" size="lg" class="spa-text-light"/>
-        </div>
         <div class="menu">
             <router-link v-for="item in menuItems" :key="item.path" :to="item.path" class="menu-link">
                 {{ item.name }}
             </router-link>
         </div>
         <div class="login-button">
-            <button v-if="!loggedIn" @click="login" class="login-btn">Log In</button>
-            <button v-else @click="logout" class="logout-btn">Log Out</button>
+            <router-link class="login-btn" v-if="!loggedIn" to="/log-in">
+                Log In
+            </router-link>
+            <q-btn v-else @click="logout" class="logout-btn">Log Out</q-btn>
         </div>
     </div>
 </template>
@@ -46,23 +45,23 @@ export default {
 
 .menu-link {
     position: relative;
-    color: #FDFFFC;
+    color: #fdfffc;
     text-decoration: none;
     transition: color 0.3s ease;
 }
 
 .menu-link:hover {
-    color: #2EC4B6;
+    color: #2ec4b6;
 }
 
 .menu-link::after {
-    content: '';
+    content: "";
     position: absolute;
     bottom: -3px;
     left: 0;
     width: 100%;
     height: 2px;
-    background-color: #2EC4B6;
+    background-color: #2ec4b6;
     transform: scaleX(0);
     transition: transform 0.3s ease;
 }
@@ -71,10 +70,10 @@ export default {
     transform: scaleX(1);
 }
 
-
 .menu {
     display: flex;
     gap: 2rem;
+    flex: 1;
 }
 
 .login-button {
@@ -86,11 +85,12 @@ export default {
 .login-btn,
 .logout-btn {
     padding: 0.5rem 1rem;
-    background-color: #2EC4B6;
-    color: #FDFFFC;
+    background-color: #2ec4b6;
+    color: #fdfffc;
     border: none;
     border-radius: 4px;
     cursor: pointer;
+    text-decoration: none;
 }
 
 
